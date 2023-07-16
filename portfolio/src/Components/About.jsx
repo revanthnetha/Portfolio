c008a98cbd4fe8ac83694677e2aade7cd4934126
Bandi Revanth <bandirevanth143@gmail.com>
2023-07-17 02:13:46 +0530

    circle

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -1,15 +1,23 @@
 import "./About.css";
-import React from "react";
+import React,{ useEffect } from "react";
 
-const toggle = document.querySelector(".main_img");
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("active");
-});
+// const toggle = document.querySelector(".main_img");
+// toggle.addEventListener("click", () => {
+//   toggle.classList.toggle("active");
+// });
 function About() {
+  useEffect(() => {
+    const toggle = document.querySelector(".main_img");
+    if (toggle) {
+      toggle.addEventListener("click", () => {
+        toggle.classList.toggle("active");
+      });
+    }
+  }, []);
   return (
     
     <div className="container home_container" >
-      
+       {/* <img className="main_img" src="path/to/image.jpg" alt="Main Image" /> */}
       <div className="logo">
         <div className="main_img">
           <span className="circle"></span>
@@ -25,6 +33,7 @@ function About() {
           src="https://res.cloudinary.com/dvcksw7qc/image/upload/v1685947913/Portfolio/photo-1519681393784-d120267933ba_hdnayh.avif"
           alt=""
         />
+        
       </div>
       
       
